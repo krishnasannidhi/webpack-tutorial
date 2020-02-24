bfbedd6e445066d57e32565000a79c7f645b455b
k0s02oi <krishnamurthy.sannidhi@walmart.com>
2020-02-23 21:59:02 -0600

    added  css loader and  style loader

---
+++ assetManagement/src/index.js
@@ -0,0 +1,5 @@
+   import './styles.css'
+   
+   const  appContainerElement = document.getElementById("appContainer")
+   appContainerElement.innerHTML= 'Welcome to webpack  tutorial ';
+   appContainerElement.classList.add('hello')
