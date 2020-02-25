dd7dc458556f284f1f3e992f643cbd8b5a0c434b
k0s02oi <krishnamurthy.sannidhi@walmart.com>
2020-02-24 19:01:04 -0600

    added development  mode  and inline source  map

---
+++ debugging_7/webpack.config.js
@@ -0,0 +1,22 @@
+const path =  require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        webpackDemoBundle: './src/index.js',
+        print: './src/print.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+    devtool: 'inline-source-map',
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ]
+}
